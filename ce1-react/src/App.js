106e47fb8cb70723cdc8403ab30cfad877c7ca9f
Joey Souza <souzajoey@gmail.com>
2025-01-04 11:17:49 -0500

    Added CSS Tailwind to prject

--- ce1-react/src/App.js
+++ ce1-react/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import './index.css'
 import Nav from './components/nav';
 import Promo from './components/promo';
 import Intro1 from './components/intro1';
@@ -10,33 +11,7 @@ import Footer from './components/footer';
 
 
 function App() {
-  return (
-  <div>
-  <div className = "main-title">
-   < Nav />
-  </div>
-
-  <div className = "promo-1">
-   < Promo />
-  </div>
-
-  <div className = "intro-1" >
-   < Intro1 reaction = "Woohoo!"/>
-  </div>
-
-  <div className = "intro-2">
-   < Intro2 reaction = "Curious?!"/>
-  </div>
-
-  <div className = "intro-3">
-   < Intro3 reaction = "HMM??!!"/>
-  </div>
-
-  <div className = "footer-main">
-   < Footer />
-  </div>
-  </div>
-);
+  return 
 }
 
 export default App;
